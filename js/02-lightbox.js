d7e7a7ed5b0ae104b2f9ff155e7481312f02981b
CodingwithLucy <143715585+CodingwithLucy@users.noreply.github.com>
2024-01-29 12:08:27 +0100

    add

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+
+const fullGallery = galleryItems.map((galleryItem) => 
+    `<a class="gallery__item" href="${galleryItem.original}">
+      <img
+        class="gallery__image"
+        src="${galleryItem.preview}"
+        alt="${galleryItem.description}"/>
+    </a>`
+  ).join("");
+
+gallery.insertAdjacentHTML("afterbegin", fullGallery);
+
 console.log(galleryItems);
